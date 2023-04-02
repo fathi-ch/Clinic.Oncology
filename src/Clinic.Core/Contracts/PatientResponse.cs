0360d208712842c7dc14a834af9ce0e160cda488
Fathi Chabane <chbnfathi@hotmail.com>
2023-04-02 15:24:29 +0200

    Using extension Method to calculate Age

--- src/Clinic.Core/Contracts/PatientResponse.cs
+++ src/Clinic.Core/Contracts/PatientResponse.cs
@@ -9,7 +9,7 @@ namespace Clinic.Core.Contracts
         public Guid Id { get; set; }   
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
-        public int Age  => (int)(DateTime.UtcNow.Subtract(BirthDate).TotalDays / 365.25);
+        public int Age { get; set; }
         public  DateTime BirthDate { get; set; }
         public  DateTime NextAppointment { get; set; }
         public string GetPatientFullName() => string.Concat(FirstName," " , LastName);
