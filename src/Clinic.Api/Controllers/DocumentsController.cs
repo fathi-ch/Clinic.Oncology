1cd55c3af5ea942b21c97647db3d9fb5f6bfe853
Fathi Chabane <chbnfathi@hotmail.com>
2023-08-19 18:54:42 +0200

    Revert "Revert "Adding Delete functionality and code refactor""
    This reverts commit 6fc829321be1314e402c10a5c8af2092f8368478.

---
+++ src/Clinic.Api/Controllers/DocumentsController.cs
@@ -0,0 +1,31 @@
+﻿using Clinic.Core.Contracts;
+using Clinic.Core.Mappers;
+using Clinic.Core.Repositories;
+using Clinic.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clinic.Api.Controllers;
+
+[ApiController]
+[Route("/v1/api/patient/{id}/documents")]
+public class DocumentsController : ControllerBase
+{
+    private readonly IDocumentRepository _document;
+
+    public DocumentsController(IDocumentRepository document)
+    {
+        _document = document;
+    }
+
+    [HttpGet(Name = "GetPatientDocumentById")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<IEnumerable<PatientDocumentResponse>>> GetPatientDocumentById(string id)
+    {
+        var patientDocuments = await _document.GetPatientDocumentByPatientIdAsync(id);
+        var documents = patientDocuments.ToList();
+        if (!documents.Any()) return NotFound();
+
+        return Ok(documents.Select(d => d.ToDocumentResponse()));
+    }
+}
