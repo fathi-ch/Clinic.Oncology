bb47bde4d9f0b7956203caa07a22aa2782d38fb7
Fathi Chabane <chbnfathi@hotmail.com>
2024-05-19 18:41:14 +0200

    Initial pdf export endpoint preparation

--- src/Clinic.Api/Controllers/DocumentsController.cs
+++ src/Clinic.Api/Controllers/DocumentsController.cs
@@ -2,6 +2,11 @@
 using Clinic.Core.Dto;
 using Clinic.Core.Services;
 using Microsoft.AspNetCore.Mvc;
+using QuestPDF.Infrastructure;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Previewer;
+using System.IO;
 
 namespace Clinic.Api.Controllers;
 
@@ -69,6 +74,72 @@ public class DocumentsController : ControllerBase
         return Ok(patientDocumentResponse);
     }
 
+    [HttpGet("export/{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> ExportDocumentsAsPdf(int id)
+    {
+
+        var documentData = await _documentRepository.GetByIdAsync(id);
+        if (documentData == null) return NotFound("Document not found.");
+
+        try
+        {
+            QuestPDF.Settings.License = LicenseType.Community;
+
+            byte[] imageBytes = null;
+            if (!string.IsNullOrEmpty(documentData.PatientDocumentsbase64))
+            {
+                imageBytes = Convert.FromBase64String(documentData.PatientDocumentsbase64);
+            }
+            
+           
+
+           var repport = Document.Create(container =>
+            {
+                _ = container.Page(page =>
+                {
+                    page.Size(PageSizes.A4);
+                    page.Margin(1, Unit.Centimetre);
+
+                    page.Header()
+
+                    .Text("CABINET D'ONCOLOGIE MEDICALE")
+                    .SemiBold().FontSize(15).FontColor(Colors.Brown.Darken1).Underline().AlignCenter();
+
+
+
+                    page.Content()
+                    .PaddingVertical(1, Unit.Centimetre)
+                    .PaddingHorizontal(1, Unit.Centimetre)
+                    .Column(x =>
+                    {
+                        //x.Spacing(10);
+                        x.Item().Text("DR AMRANE.L");
+                        x.Item().Text("ONCOLOGUE MEDICALE");
+                        x.Spacing(10);
+                        // x.Item().Image(imageBytes, ImageScaling.FitWidth);
+                        x.Item().Image(imageBytes).FitHeight();
+
+                    });
+
+
+                });
+            })            
+            .GeneratePdf();
+
+            
+            return File(repport, "application/pdf", $"Document_{documentData.Name}.pdf");            
+        }
+        catch (Exception ex)
+        {
+            // Log and handle general exceptions
+            Console.WriteLine("Exception: " + ex.Message);
+            Console.WriteLine("Stack Trace: " + ex.StackTrace);
+            return StatusCode(StatusCodes.Status500InternalServerError, new { Message = "Error while generating PDF document." });
+        }
+    }
+
     [HttpDelete("{id}", Name = "DeleteDocument")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -101,7 +172,8 @@ public class DocumentsController : ControllerBase
     {
         if (!ModelState.IsValid)
         {
-            return BadRequest(ModelState); }
+            return BadRequest(ModelState);
+        }
 
         var document = await _documentRepository.GetByIdAsync(id);
         if (document == null) return NotFound();
@@ -109,5 +181,38 @@ public class DocumentsController : ControllerBase
         var documentInDb = await _documentRepository.UpdateByIdAsync(id, patientDocumentDto);
 
         return Ok(documentInDb);
+    }
+
+    private Document GeneratePdfDocument(PatientDocumentResponse documentData)
+    {
+        return null;
+
+        // return Document.Create(container =>
+        // {
+        //     container.Page(page =>
+        //     {
+        //         page.Size(PageSizes.A4);
+        //         page.Margin(2, Unit.Centimetre);
+        //         page.DefaultTextStyle(x => x.FontSize(20));
+
+        //         page.Content()
+        //             .Column(column =>
+        //             {
+        //                 column.Item().Text($"Exported Document: {documentData.Name}").Bold().FontSize(24);
+        //                 column.Item().Text($"Visit ID: {documentData.VisitId}");
+        //                 column.Item().Text($"Document Type: {documentData.DocumentType}");
+
+        //                 if (!string.IsNullOrEmpty(documentData.PatientDocumentsbase64))
+        //                 {
+        //                     byte[] documentBytes = Convert.FromBase64String(documentData.PatientDocumentsbase64);
+        //                     using (var documentStream = new MemoryStream(documentBytes))
+        //                     {
+        //                         var image = ImageSource.FromStream(documentStream);
+        //                         column.Item().Image(image);
+        //                     }
+        //                 }
+        //             });
+        //     });
+        // });
     }
 }
